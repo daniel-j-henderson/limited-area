f19db11df912c7970d868583864a76c1596bf3f6
daniel-j-henderson <hendersn@ucar.edu>
2016-09-26 14:04:08 -0600

    Restructured build dir

---
+++ src/params.f90
@@ -0,0 +1,4 @@
+module params
+   integer, parameter :: RKIND=8, StrKIND=100, MAX_NDIMS=30, MAX_NVARS=150, MAX_NATTS=15, DIM=1, VAR=2, ATT=3, NVARS_MAX_NAMELIST=20
+   real(kind=RKIND), parameter :: PI = 3.14159265359
+end module params
